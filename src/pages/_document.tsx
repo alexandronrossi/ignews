e85167fff7177ca4fd1f41b54e993edf146d0e21
Alexandro <alexandronrossi@hotmail.com>
2022-01-16 22:25:23 -0300

    Finalizando o módulo de estruturação da aplicação

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,18 +5,16 @@ export default class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-                        <link rel="preconnect" href="https://fonts.googleapis.com">
-                            <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Roboto:wght@400;700;900&display=swap" rel="stylesheet">
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
+                </Head>
 
-                                <title>ig.news</title>
-                            </Head>
-
-                            <body>
-                                <Main />
-                                <NextScript />
-                            </body>
-                        </Html>
-                        );
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
     }
 }
